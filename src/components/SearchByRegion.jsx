8f57fc0991b53fdf52032636d2c8f13b541f3a8f
devKizito <devkizito@gmail.com>
2022-05-28 23:17:26 +0100

    updated UI of Search and Filter by Region

--- src/components/SearchByRegion.jsx
+++ src/components/SearchByRegion.jsx
@@ -21,20 +21,6 @@ const SearchByRegion = ({ setSearch }) => {
 
   return (
     <div className="w-full lg:w-1/6">
-      {/* <select
-        className="text-slate-800 py-4 px-6 rounded-sm bg-slate-50 dark:bg-slate-800 border dark:border-slate-500 dark:text-slate-500 w-full"
-        onChange={(e) => setCurrentRegion(e.target.value)}
-      >
-        <option selected value={""} default>
-          Filter By Region
-        </option>
-        {regions.map((region, index) => (
-          <option key={index} value={region} className="text-slate-800">
-            {region}
-          </option>
-        ))}
-      </select> */}
-
       <Listbox value={currentRegion} onChange={setCurrentRegion}>
         {({ open }) => (
           <>
